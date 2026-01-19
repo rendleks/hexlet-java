a87d6330798c7595efb7177decce9a4af2033a07
rendleks <0rendl050@gmail.com>
2026-01-19 11:59:18 +0300

    update One.java

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,7 +1,10 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+        var reversWord = StringUtils.reverse("hexlet");
+        System.out.println(reversWord);
     }
 }
